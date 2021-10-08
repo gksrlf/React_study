d258e4b03a2bb72e86962721ea9b44f27f744e22
hangil <59948163+gksrlf@users.noreply.github.com>
2021-10-08 10:44:43 +0900

    211008mornig

---
+++ tictactoe/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from '.'
+
+const App = () => {
+    return (
+        <div>
+                <Game/>
+        </div>
+    );
+}
